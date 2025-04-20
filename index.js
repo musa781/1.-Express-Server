14df449b0d2c338085e710fc72b2f8a302bef7e3
Muhammad Usama Sharif <112772227+musa781@users.noreply.github.com>
2025-04-20 16:13:25 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req) => {
+
+    req.send("hello world");
+
+
+});
+
+app.listen(port, () => {
+
+    console.log(`server is running on port ${port}.`);
+});
